22bb1ed70979f374c3c83a12209eaa0eda921167
yinxing <yinxing@dxy.cn>
2020-08-26 13:16:33 +0800

    feat: 新增 tomcat 15 节代码练习

--- src/main/java/com/example/demo/tomcatColumn/lecture15/java_nio/AcceptHandler.java
+++ src/main/java/com/example/demo/tomcatColumn/lecture15/java_nio/AcceptHandler.java
@@ -5,7 +5,8 @@ import java.nio.channels.AsynchronousSocketChannel;
 import java.nio.channels.CompletionHandler;
 
 /**
- * AcceptHandler类实现了CompletionHandler接口的completed方法。它还有两个模板参数，第一个是异步通道，第二个就是Nio2Server本身
+ * AcceptHandler类实现了 CompletionHandler 接口的 completed 方法。
+ * 它还有两个模板参数，第一个是异步通道，第二个就是 Nio2Server 本身
  *
  * @author Alan Yin
  * @date 2020/6/19
@@ -14,14 +15,14 @@ import java.nio.channels.CompletionHandler;
 public class AcceptHandler implements CompletionHandler<AsynchronousSocketChannel, Nio2Server> {
 
     /**
-     * 具体处理连接请求的就是completed方法，它有两个参数：第一个是异步通道，第二个就是上面传入的NioServer对象
+     * 具体处理连接请求的就是 completed 方法，它有两个参数：第一个是异步通道，第二个就是上面传入的NioServer对象
      *
      * @param result
      * @param attachment
      */
     @Override
     public void completed(AsynchronousSocketChannel result, Nio2Server attachment) {
-        //调用accept方法继续接收其他客户端的请求
+        //调用 accept 方法继续接收其他客户端的请求
 //        attachment.assc.accept(attachment, this);
 
         // 1.先分配好 Buffer,告诉内核，数据拷贝到哪里去
--- src/main/java/com/example/demo/tomcatColumn/lecture15/java_nio/Nio2Server.java
+++ src/main/java/com/example/demo/tomcatColumn/lecture15/java_nio/Nio2Server.java
@@ -1,6 +1,6 @@
 package com.example.demo.tomcatColumn.lecture15.java_nio;
 
-import com.example.demo.tomcatColumn.lecture15.java_nio.pool.NamedThreadFactory;
+import com.example.demo.tomcatColumn.lecture15.java_nio.pool.MyThreadFactoryBuilder;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -19,16 +19,14 @@ public class Nio2Server {
 
     void listen() throws IOException {
         // 1.创建一个线程池：用来执行来自内核的回调请求
-        // ThreadFactory namedThreadFactory = new MyThreadFactoryBuilder().setPoolName("my-thread").build();
-        ThreadFactory namedThreadFactory = new NamedThreadFactory();
-
+        ThreadFactory namedThreadFactory = new MyThreadFactoryBuilder().setPoolName("my-thread").build();
+        // ThreadFactory namedThreadFactory = new NamedThreadFactory();
         ExecutorService pool = new ThreadPoolExecutor(5, 20,
                 0L, TimeUnit.MILLISECONDS,
                 new LinkedBlockingQueue<>(1024), namedThreadFactory, new ThreadPoolExecutor.AbortPolicy());
-        ExecutorService es = Executors.newCachedThreadPool();
 
         // 2.创建一个异步通道群组，绑定一个线程池
-        AsynchronousChannelGroup acg = AsynchronousChannelGroup.withCachedThreadPool(es, 1);
+        AsynchronousChannelGroup acg = AsynchronousChannelGroup.withCachedThreadPool(pool, 1);
 
         // 3.创建服务端异步通道
         AsynchronousServerSocketChannel assc = AsynchronousServerSocketChannel.open(acg);
